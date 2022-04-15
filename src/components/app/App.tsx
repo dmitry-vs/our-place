e24d062d6ad2c0281124f3d39628b619724069f8
dmitry-vs <dmitryselin@inbox.ru>
2022-04-15 09:26:50 +0300

    Добавлена поддержка emotion (styled, css)

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import clsx from 'clsx';
 import './App.css';
-import s1 from './App.module.css';
-import s2 from './App.module.scss';
-import s3 from './App.module.sass';
+import stylesCss from './App.module.css';
+import stylesScss from './App.module.scss';
+import stylesSass from './App.module.sass';
+import styled from '@emotion/styled';
+import { css } from '@emotion/css';
+
+const Div = styled.div`
+  color: blueviolet;
+`;
 
 const App = () => (
-  <h1 className={clsx('App', s1.App, s2.App, s3.App)}>My React App</h1>
+  <>
+    <h1>My React App</h1>
+    <div className="header">Styled with css (plain)</div>
+    <div className={stylesCss.text}>Styled with css (modules)</div>
+    <div className={stylesScss.text}>Styled with scss (modules)</div>
+    <div className={stylesSass.text}>Styled with sass (modules)</div>
+    <Div>Styled with @emotion/styled</Div>
+    <div
+      className={css`
+        font-family: monospace;
+      `}
+    >
+      Styled with @emotion/css
+    </div>
+  </>
 );
 
 export default App;
