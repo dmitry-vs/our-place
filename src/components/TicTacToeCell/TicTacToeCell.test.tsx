6b4401bab0ee1f2d540112048a20707d8dd25399
dmitry-vs <dmitryselin@inbox.ru>
2022-04-16 11:39:52 +0300

    (TicTacToeCell) - добавлен click handler

--- src/components/TicTacToeCell/TicTacToeCell.test.tsx
+++ src/components/TicTacToeCell/TicTacToeCell.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import TicTacToeCell from './TicTacToeCell';
 import { render, screen } from '@testing-library/react';
 import { TicTacToeCellValues } from '../../helpers/consts';
+import userEvent from '@testing-library/user-event';
 
 describe('TicTacToeCell', () => {
   const role = 'tic-tac-toe-cell';
@@ -19,5 +20,21 @@ describe('TicTacToeCell', () => {
   it('renders circle sign if value is circle', () => {
     render(<TicTacToeCell value={TicTacToeCellValues.Circle} />);
     expect(screen.getByRole(role)).toHaveTextContent('O');
+  });
+
+  it('calls handleClick function when clicked', async () => {
+    const handleClick = jest.fn();
+    const user = userEvent.setup();
+    render(
+      <TicTacToeCell
+        value={TicTacToeCellValues.Empty}
+        handleClick={handleClick}
+      />
+    );
+
+    const cell = screen.getByRole(role);
+    await user.click(cell);
+
+    expect(handleClick).toHaveBeenCalled();
   });
 });
--- src/components/TicTacToeCell/TicTacToeCell.tsx
+++ src/components/TicTacToeCell/TicTacToeCell.tsx
@@ -1,18 +1,23 @@
-import React, { FC } from 'react';
+import React, { FC, MouseEventHandler } from 'react';
 import { TicTacToeCellValues } from '../../helpers/consts';
 
 type TicTacToeCellProps = {
   value: TicTacToeCellValues;
+  handleClick?: MouseEventHandler;
 };
 
-const TicTacToeCell: FC<TicTacToeCellProps> = ({ value }) => {
+const TicTacToeCell: FC<TicTacToeCellProps> = ({ value, handleClick }) => {
   const cellContentMapping = {
     [TicTacToeCellValues.Empty]: '',
     [TicTacToeCellValues.Cross]: 'X',
     [TicTacToeCellValues.Circle]: 'O',
   };
 
-  return <div role="tic-tac-toe-cell">{cellContentMapping[value]}</div>;
+  return (
+    <div role="tic-tac-toe-cell" onClick={handleClick}>
+      {cellContentMapping[value]}
+    </div>
+  );
 };
 
 export default TicTacToeCell;
